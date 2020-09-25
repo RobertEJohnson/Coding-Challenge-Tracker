34d78ea5edc997a1bd31cf003e51508b1591dfb6
Rob Wolfe <robertejohnson10@gmail.com>
2020-09-25 13:57:25 -0500

    Replace bootstrap navbar with MDBNavbar, added react router dom

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,35 @@
 import React, {Component} from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
-
+import { MDBCollapse, MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBNavItem, MDBNavLink } from 'mdbreact';
+import {BrowserRouter as Router} from 'react-router-dom';
 class ReactBootstrapNavbar extends Component{
+    state={
+        open: false
+    }
+
+    toggleCollapse=()=>{
+        this.setState({
+            open: !this.state.open
+        })
+    }
+    
     render(){
+        let anchorStyle ={
+            textDecoration: 'none'
+        }
         return(
-            <Navbar bg="light" expand="lg" fixed="top">
-                <Navbar.Brand href="#home">Edabit Learning</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="https://github.com/RobertEJohnson" target="_blank">Rob's GitHub Portfolio</Nav.Link>
-                        <NavDropdown title="Contact Me" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <Router>
+                <MDBNavbar expand='lg'>
+                    <MDBNavbarBrand>Edabit Learning</MDBNavbarBrand>
+                    <MDBNavbarToggler className='navbar-light' onClick={this.toggleCollapse} />
+                    <MDBCollapse navbar isOpen={this.state.open}>
+                        <MDBNavbarNav right>
+                        <MDBNavItem>
+                            <a style={anchorStyle} href="https://github.com/RobertEJohnson" target='_blank'>Rob's GitHub Portfolio</a>
+                        </MDBNavItem>
+                        </MDBNavbarNav>
+                    </MDBCollapse>
+                </MDBNavbar>
+            </Router>
         )
     }
 }
