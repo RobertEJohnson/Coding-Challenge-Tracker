ab13d7a7bfbdec7a483575c91a61002759dd7922
Rob Wolfe <robertejohnson10@gmail.com>
2020-09-28 17:54:25 -0500

    Condensed code into a map from local state.

--- src/components/ChallengesContainer/ChallengesContainer.js
+++ src/components/ChallengesContainer/ChallengesContainer.js
@@ -1,47 +1,37 @@
 import React, {Component} from 'react';
-import { MDBCol, MDBRow, MDBContainer,  } from "mdbreact";
+import { MDBRow, MDBContainer} from "mdbreact";
 import CustomCard from '../Card/Card';
 
 class ChallengesContainer extends Component{
-    render(){
-        return(
-            <MDBContainer>
-                <MDBRow className="mb-4">
-                  <MDBCol xs={12} md={4}>
-                    <CustomCard animation='fadeInLeft' date='September 24th 2020' title='Edabit Challenge' text=''/>
-                  </MDBCol>
-                  <MDBCol xs={12} md={4}>
-                    <CustomCard animation='fadeIn' date='September 24th 2020' title='Edabit Challenge' text=''/>
-                  </MDBCol>
-                  <MDBCol xs={12} md={4}>
-                    <CustomCard animation='fadeInRight' date='September 24th 2020' title='Edabit Challenge' text=''/>
-                  </MDBCol>
-                </MDBRow>
-                <MDBRow className="mb-4">
-                  <MDBCol xs={12} md={4}>
-                    <CustomCard animation='fadeInLeft' date='September 24th 2020' title='Edabit Challenge' text=''/>
-                  </MDBCol>
-                  <MDBCol xs={12} md={4}>
-                    <CustomCard animation='fadeInUp' date='September 24th 2020' title='Edabit Challenge' text=''/>
-                  </MDBCol>
-                  <MDBCol xs={12} md={4}>
-                    <CustomCard animation='fadeInRight' date='September 24th 2020' title='Edabit Challenge' text=''/>
-                  </MDBCol>
-                </MDBRow>
-                <MDBRow className="mb-4">
-                  <MDBCol xs={12} md={4}>
-                    <CustomCard animation='fadeInLeft' date='September 24th 2020' title='Edabit Challenge' text=''/>
-                  </MDBCol>
-                  <MDBCol xs={12} md={4}>
-                    <CustomCard animation='fadeInUp' date='September 24th 2020' title='Edabit Challenge' text=''/>
-                  </MDBCol>
-                  <MDBCol xs={12} md={4}>
-                    <CustomCard animation='fadeInRight' date='September 24th 2020' title='Edabit Challenge' text=''/>
-                  </MDBCol>
-                </MDBRow>
-              </MDBContainer>
-        )
-    }
+  state={
+    months: [
+      {name: 'October'}, 
+      {name: 'November'},
+      {name: 'December'},
+      {name: 'January'},
+      {name: 'February'},
+      {name: 'March'},
+      {name: 'April'},
+      {name: 'May'},
+      {name: 'June'},
+      {name: 'July'},
+      {name: 'August'},
+      {name: 'September'},
+    ]
+  }
+  render(){
+    return(
+      <MDBContainer>
+        <MDBRow>
+          {
+            this.state.months.map(month=>{
+                return <CustomCard date={month.name}/>;
+            })    
+          }
+        </MDBRow>
+      </MDBContainer>
+      )
+  }
 }
 
 export default ChallengesContainer;
